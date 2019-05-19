f20511e92cd532bd559097d0278b7785c61d3605
Daniel Lundén <dlunde@kth.se>
2019-05-19 19:25:19 +0200

    Add examples compare dynamic and direct smc

--- src/debug.ml
+++ src/debug.ml
@@ -19,7 +19,7 @@ let debug_resample_transform = false
 let debug_cps = false
 
 (** Debug SMC inference *)
-let debug_smc = true
+let debug_smc = false
 
 (** Debug dynamic SMC inference *)
 let debug_smc_dyn = false
