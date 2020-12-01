1313ff9a62318156b01e8826ad9a7b4ea3c18b39
Viktor Senderov <38105253+vsenderov@users.noreply.github.com>
2020-12-01 14:42:17 +0100

    Update crbd.R

--- treeppl/examples/crbd.R
+++ treeppl/examples/crbd.R
@@ -4,6 +4,9 @@ tree = phyjson.read(filename="some PhyJSON file")
 k_lambda = 1
 theta_lambda = 0.2
 
+k_mu = 1
+theta_mu = 0.1
+
 ppl = readLines(file = "crbd.tppl")
 
 result = treeppl(ppl, input = list(k_lambda, k_theta, k_mu, theta_mu, tree), method = "SMC", particles = "10000" )
