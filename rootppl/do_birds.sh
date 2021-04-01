98738fd915a218e6835ae1d0610a121ce2c3f93a
Viktor Senderov <vsenderov@gmail.com>
2021-04-01 18:20:58 +0300

    A script to run all bird analyses.

---
+++ rootppl/do_birds.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+BIRDDIR=/home/viktor/ownCloud/miking-dppl/rootppl/models/phylogenetics/birds
+NUMPART=$1
+NUMRUN=$2
+CORES=32
+RESULTDIR=results/birds-$NUMPART
+if [ ! -f $RESULTDIR ]
+then
+    mkdir $RESULTDIR
+fi
+# test var needs to be set 
+for model in $(ls $BIRDDIR)
+do
+    make clean
+    make model=$BIRDDIR/$model -j$CORES
+    ./program $NUMPART $NUMRUN
+    cp log_norm_const.txt $RESULTDIR/$model.logz
+done
