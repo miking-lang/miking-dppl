c603cae65cb1734e91a2a7cec502260347011c70
Daniel Lundén <dlunde@kth.se>
2021-10-28 11:24:40 +0200

    Finalize switch from boot to mi

--- test.mk
+++ test.mk
@@ -11,7 +11,7 @@ all: ${test-files}
 
 # TODO(dlunde,2021-10-25): We are getting lots of name collisions with `mi`, hence the temp dir. Could be solved by having an `-o` flag for `mi`
 ${test-files}::
-	@mkdir test_tmp
-	@cd test_tmp && ../make test ../$@
-	@rmdir test_tmp
+	@mkdir test-$(subst /,-,$@)
+	@cd test-$(subst /,-,$@) && ../make test ../$@
+	@rmdir test-$(subst /,-,$@)
 
