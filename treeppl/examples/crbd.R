e263dd102ad06813752e75f5953a910a45a75cdc
Viktor Senderov <38105253+vsenderov@users.noreply.github.com>
2022-02-15 16:34:40 +0200

    Update crbd.R

--- treeppl/examples/crbd.R
+++ treeppl/examples/crbd.R
@@ -1,17 +1,31 @@
 # Imaginary R syntax to read to invoke TreePPL form R
 
-tree = phyjson.read(filename="some phyjson file")
+library(treeppl)
+
+# INPUT
+tree = treeppl.readphyjson(filename="some phyjson file")
 k_lambda = 1
 theta_lambda = 0.2
 
 ppl = readLines(file = "crbd.tppl")
+result = treeppl(ppl, input = list(k_lambda, k_theta, k_mu, theta_mu, tree))
 
-result = treeppl(ppl, input = list(k_lambda, k_theta, k_mu, theta_mu, tree) )
-
-result$lambda
-result$mu
+# OUTPUT
+# result is a list
+# lambda and mu are data.frames
+# normconst is a numeric
+result$lambda$values 
+result$lambda$weights
+result$mu$values
+result$mu$weights
 result$normconst
 
-plot(result$lambda)
+# delayed sampling
+result$lambda_k$values
+result$lambda_theta$weights
+result$mu_k$values
+result$mu_theta$weights
+result$normconst
 
-
+# importance sampling
+# plotting
